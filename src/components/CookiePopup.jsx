2a1bba0ee7da900a2be9ca63d7a6e2be6baf70e9
Fridakea <frbe0002@stud.kea.dk>
2024-10-29 09:54:17 +0100

    done

--- src/components/CookiePopup.jsx
+++ src/components/CookiePopup.jsx
@@ -3,15 +3,19 @@ import { LiaCookieBiteSolid } from "react-icons/lia";
 const CookiePopup = () => {
   return (
     <section className="bg-white w-[100vw] h-[100vh]">
-      <div className="w-fit bg-black p-4 rounded-xl">
-        <div className="flex flex-row">
-          <LiaCookieBiteSolid className="text-red-500 fill-green-600" />
-          <h2>x</h2>
+      <div className="w-[300px] fixed bottom-0 right-0 bg-black py-2 px-4 rounded-xl">
+        <div className="mb-16 flex flex-row justify-between">
+          <LiaCookieBiteSolid className="w-16 h-16 mt-4 ml-3 scale-x-[-1] fill-gray-200" />
+          <p className="text-gray-400 text-xl">x</p>
         </div>
-        <p>We use Cookies to improve your service experience.</p>
-        <button className="w-[100%] text-black bg-white  py-4 rounded-xl">
-          I like Cookies
-        </button>
+        <div className="py-4 px-4">
+          <p className="text-xl mb-6 font-sans">
+            We use cookies to improve your user experience.
+          </p>
+          <button className="w-[100%] text-black bg-gray-200 py-3 rounded-xl text-xl font-sans">
+            I like Cookies
+          </button>
+        </div>
       </div>
     </section>
   );
